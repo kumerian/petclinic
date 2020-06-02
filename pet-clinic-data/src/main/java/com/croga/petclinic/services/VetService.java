8712cbd0abf2e9f6ae35abe6b68c1cbf8baea911
Koen van der Pasch <croga@croga.com>
2020-06-02 08:05:20 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/croga/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.croga.petclinic.services;
+
+import com.croga.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
