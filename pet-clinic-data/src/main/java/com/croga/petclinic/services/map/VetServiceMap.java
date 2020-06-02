f3bd499a84fd94e44e36d3460ba22b38d6c97bc8
Koen van der Pasch <croga@croga.com>
2020-06-02 11:24:38 +0200

    implement Map based services. closes #6

---
+++ pet-clinic-data/src/main/java/com/croga/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.croga.petclinic.services.map;
+
+import com.croga.petclinic.model.Vet;
+import com.croga.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbastractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
