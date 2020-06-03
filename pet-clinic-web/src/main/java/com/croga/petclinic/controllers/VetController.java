cd8290e5e8c30ffd06935684ba7b1bf000f73c49
Koen van der Pasch <croga@croga.com>
2020-06-03 08:01:51 +0200

    adding vets list and updating controller. Closes #15

--- pet-clinic-web/src/main/java/com/croga/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/croga/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.croga.petclinic.controllers;
 
+import com.croga.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/", "/vets/index.html", "/vets/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
