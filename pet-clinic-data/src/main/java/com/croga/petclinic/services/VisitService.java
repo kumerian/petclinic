afd111c14aa7dbc3d3165a7e82df508f0ecb9110
Koen van der Pasch <croga@croga.com>
2020-06-09 19:54:15 +0200

    Implemented Map implementation for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/croga/petclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.croga.petclinic.services;
+
+import com.croga.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/croga/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.croga.petclinic.services.map;
+
+import com.croga.petclinic.model.Visit;
+import com.croga.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbastractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
