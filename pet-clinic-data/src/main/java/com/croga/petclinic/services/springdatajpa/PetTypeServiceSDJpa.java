f1e6636086b5ea4e862bcf44bfabb66c504321c4
Koen van der Pasch <croga@croga.com>
2020-06-09 19:27:31 +0200

    Added Spring Data JPA service for PetTypes. Closes #42

---
+++ pet-clinic-data/src/main/java/com/croga/petclinic/services/springdatajpa/PetTypeServiceSDJpa.java
@@ -0,0 +1,48 @@
+package com.croga.petclinic.services.springdatajpa;
+
+import com.croga.petclinic.model.PetType;
+import com.croga.petclinic.repositories.PetTypeRepository;
+import com.croga.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeServiceSDJpa implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceSDJpa(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
