497f46a552bacee35c0ed64dd6bb6d9cb6d97077
Koen van der Pasch <croga@croga.com>
2020-06-10 19:35:21 +0200

    Added tests for OwnerController with MockMvc. Closes #52

--- pet-clinic-web/src/main/java/com/croga/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/croga/petclinic/controllers/OwnerController.java
@@ -20,7 +20,7 @@ public class OwnerController {
 
         model.addAttribute("owners", ownerService.findAll());
 
-        return "owners/index.html";
+        return "owners/index";
     }
 
     @RequestMapping("/find")
