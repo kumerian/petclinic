6dfd78ec48ae8fccc76080ad4ce6b85600962e46
Koen van der Pasch <croga@croga.com>
2020-06-16 15:09:39 +0200

    Fixed display Owner. Closes #61

--- pet-clinic-web/src/main/java/com/croga/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/croga/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.croga.petclinic.controllers;
 import com.croga.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -26,6 +29,13 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(ownerId));
+        return modelAndView;
     }
 
 }
