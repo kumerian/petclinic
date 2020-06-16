94a03e3ef962a9e4f3cbac3bb9dfa88d8c547cdd
Koen van der Pasch <croga@croga.com>
2020-06-16 16:55:29 +0200

    Implemented Find Owner. Closes #54

--- pet-clinic-data/src/main/java/com/croga/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/croga/petclinic/services/map/OwnerServiceMap.java
@@ -8,6 +8,7 @@ import com.croga.petclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -30,6 +31,13 @@ public class OwnerServiceMap extends AbastractMapService<Owner, Long> implements
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+
+        //todo - implement
+        return null;
     }
 
     @Override
--- pet-clinic-web/src/test/java/com/croga/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/croga/petclinic/controllers/OwnerControllerTest.java
@@ -11,11 +11,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -44,30 +47,6 @@ class OwnerControllerTest {
     }
 
     @Test
-    void listOwners() throws Exception {
-        //Mock
-        when(ownerService.findAll()).thenReturn(owners);
-
-        //Test
-        mockMvc.perform(get("/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
-    }
-
-    @Test
-    void listOwnersByIndex() throws Exception {
-        //Mock
-        when(ownerService.findAll()).thenReturn(owners);
-
-        //Test
-        mockMvc.perform(get("/owners/index"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
-    }
-
-    @Test
     void displayOwner() throws Exception {
         //Mock
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
@@ -83,8 +62,31 @@ class OwnerControllerTest {
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("notImplemented"));
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
 
         verifyNoInteractions(ownerService);
     }
+
+    @Test
+    void processFindFormReturnMany() throws Exception {
+        List<Owner> owners = Arrays.asList(Owner.builder().id(1L).build(), Owner.builder().id(2L).build());
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(owners);
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
+    void processFindFormReturnOne() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+    }
+
+
 }
