28844b599be9714f215c07919723d79354a6df76
Koen van der Pasch <croga@croga.com>
2020-06-16 20:55:35 +0200

    Adding model attributes for Pet Controller. #56

---
+++ pet-clinic-web/src/main/java/com/croga/petclinic/controllers/PetController.java
@@ -0,0 +1,45 @@
+package com.croga.petclinic.controllers;
+
+import com.croga.petclinic.model.Owner;
+import com.croga.petclinic.model.PetType;
+import com.croga.petclinic.services.OwnerService;
+import com.croga.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(OwnerService ownerService, PetTypeService petTypeService) {
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("types")
+    public Set<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+}
