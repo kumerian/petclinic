8b6d15c13641883dd59d08a6432827e6ec7a42f4
croga <croga@croga.com>
2020-06-17 22:28:34 +0200

    Added JSON endpoint for Vets. Closes #67

--- pet-clinic-web/src/main/java/com/croga/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/croga/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.croga.petclinic.controllers;
 
+import com.croga.petclinic.model.Vet;
 import com.croga.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,6 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 
 }
