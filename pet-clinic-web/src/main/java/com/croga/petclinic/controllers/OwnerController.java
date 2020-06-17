31c5a0c13f1fc399ec9f4a1cfb04ea592ad9543b
Koen van der Pasch <croga@croga.com>
2020-06-17 06:05:02 +0200

    Adding forms for Pet and Visit. #56

--- pet-clinic-web/src/main/java/com/croga/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/croga/petclinic/controllers/OwnerController.java
@@ -5,11 +5,11 @@ import com.croga.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -68,7 +68,7 @@ public class OwnerController {
     }
 
     @PostMapping("/new")
-    public String processCreationForm(@Validated Owner owner, BindingResult result) {
+    public String processCreationForm(@Valid Owner owner, BindingResult result) {
         if (result.hasErrors()) {
             return OWNERS_CREATE_OR_UPDATE_OWNER_FORM;
         } else {
@@ -84,7 +84,7 @@ public class OwnerController {
     }
 
     @PostMapping("/{ownerId}/edit")
-    public String processUpdateOwnerForm(@Validated Owner owner,
+    public String processUpdateOwnerForm(@Valid Owner owner,
                                          BindingResult result,
                                          @PathVariable Long ownerId) {
         if (result.hasErrors()) {
